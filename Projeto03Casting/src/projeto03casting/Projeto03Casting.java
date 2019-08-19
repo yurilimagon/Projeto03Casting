0c2fbb30cd2d8370007bcbd86a5f5c4057e92ef0
Yuri Lima <yurilimagoncalves@gmail.com>
2019-08-19 19:44:07 -0300

    Inicialização do Projeto03Casting

---
+++ Projeto03Casting/src/projeto03casting/Projeto03Casting.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto03casting;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto03Casting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double pi = 3.14;
+        int i = (int) pi; // casting = SE MOLDE A....
+        System.out.println("I: " + i);
+        System.out.println("PI: " + pi);
+        
+        float j = 3.14f; //Utilizar f no final quando atribuir valor inicial a variável
+        float k = 3;
+        float m = (float) pi; //casting
+        
+        
+    }
+    
+}
